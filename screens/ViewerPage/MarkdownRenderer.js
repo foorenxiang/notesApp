8777a7750ed7ede11acb9e6614674cc82bfc45b4
Ren Xiang <foorenxiang@gmail.com>
2021-06-02 00:00:11 +0800

    changed web markdown rendering

--- screens/ViewerPage/MarkdownRenderer.js
+++ screens/ViewerPage/MarkdownRenderer.js
@@ -2,10 +2,17 @@ import React from 'react';
 import MarkdownNative from 'react-native-markdown-display';
 import MarkdownWeb from 'react-markdown';
 import { isNativeDevice } from '../../utils/platformDetector';
+import { Prism } from 'react-syntax-highlighter';
+import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+
+// import { coy } from 'react-syntax-highlighter/dist/styles/prism';
 
 export default ({ children: markdownData }) =>
   isNativeDevice ? (
     <MarkdownNative>{markdownData}</MarkdownNative>
   ) : (
-    <MarkdownWeb>{markdownData}</MarkdownWeb>
+    <Prism language="markdown" style={dark}>
+      {/* <MarkdownWeb>{markdownData}</MarkdownWeb> */}
+      {markdownData}
+    </Prism>
   );
